8bf10f50d5b335296403a04bf934b9ab2db1e0bc
kkun <492504509@qq.com>
2017-06-25 00:28:40 +0800

    order module

---
+++ src/page/about/index.js
@@ -0,0 +1,11 @@
+/*
+* @Author: kkun
+* @Date:   2017-06-13 19:17:44
+* @Last Modified by:   kkun
+* @Last Modified time: 2017-06-24 23:37:32
+*/
+
+'use strict';
+
+require('page/common/header/index.js');
+require('page/common/nav/index.js');
--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,40 @@
 /*
 * @Author: kkun
-* @Date:   2017-06-19 05:57:06
+* @Date:   2017-05-08 15:28:19
 * @Last Modified by:   kkun
-* @Last Modified time: 2017-06-22 20:03:22
+* @Last Modified time: 2017-06-24 23:53:49
 */
+var webpack             = require('webpack');
+var ExtractTextPlugin   = require('extract-text-webpack-plugin');
+var HtmlWebpackPlugin   = require('html-webpack-plugin');
 
-var webpack           = require('webpack');
-var ExtractTextPlugin = require("extract-text-webpack-plugin"); 
-var HtmlWebpackPlugin = require("html-webpack-plugin"); 
+// 环境变量配置，dev / online
+var WEBPACK_ENV         = process.env.WEBPACK_ENV || 'dev';
 
-//环境变量 dev / online 
-var _env = process.env._env || 'dev';
-
-//get html webpack plugin 参数
-var getHtmlConfig = function(name, title) {
+// 获取html-webpack-plugin参数的方法 
+var getHtmlConfig = function(name, title){
     return {
-        template : './src/view/' + name + '.html',
-        filename : 'view/' + name +'.html',
-        title    : title,
-        inject : true,
-        hash : true,
-        chunks : ['common', name],
+        template    : './src/view/' + name + '.html',
+        filename    : 'view/' + name + '.html',
+        favicon     : './favicon.ico',
+        title       : title,
+        inject      : true,
+        hash        : true,
+        chunks      : ['common', name]
     };
-}
-
+};
+// webpack config
 var config = {
-     entry: {
+    entry: {
         'common'            : ['./src/page/common/index.js'],
         'index'             : ['./src/page/index/index.js'],
         'list'              : ['./src/page/list/index.js'],
         'detail'            : ['./src/page/detail/index.js'],
         'cart'              : ['./src/page/cart/index.js'],
-        //'order-confirm'     : ['./src/page/order-confirm/index.js'],
+        'order-confirm'     : ['./src/page/order-confirm/index.js'],
+        'order-list'        : ['./src/page/order-list/index.js'],
+        'order-detail'      : ['./src/page/order-detail/index.js'],
+        'payment'           : ['./src/page/payment/index.js'],
         'user-login'        : ['./src/page/user-login/index.js'],
         'user-register'     : ['./src/page/user-register/index.js'],
         'user-pass-reset'   : ['./src/page/user-pass-reset/index.js'],
@@ -39,45 +42,56 @@ var config = {
         'user-center-update': ['./src/page/user-center-update/index.js'],
         'user-pass-update'  : ['./src/page/user-pass-update/index.js'],
         'result'            : ['./src/page/result/index.js'],
+        'about'             : ['./src/page/about/index.js'],
     },
-     output: {
-         path: './dist',
-         publicPath: '/dist',
-         filename: 'js/[name].js'
-     },
-     externals : {
+    output: {
+        path        : __dirname + '/dist/',
+        publicPath  : 'dev' === WEBPACK_ENV ? '/dist/' : '/dist/',
+        filename    : 'js/[name].js'
+    },
+    externals : {
         'jquery' : 'window.jQuery'
-     },
-     module: {
+    },
+    module: {
         loaders: [
-            { test: /\.css$/, loader : ExtractTextPlugin.extract("style-loader","css-loader")  },
-            { test: /\.(gif|png|jpg|jpeg|woff|svg|eot|ttf)\??.*/, loader : 'url-loader?limit=128&name=resource/[name].[ext]' },
-            { test: /\.string$/, loader: 'html-loader'}
+            { test: /\.css$/, loader: ExtractTextPlugin.extract("style-loader","css-loader") },
+            { test: /\.(gif|png|jpg|woff|svg|eot|ttf)\??.*$/, loader: 'url-loader?limit=100&name=resource/[name].[ext]' },
+            {
+                test: /\.string$/, 
+                loader: 'html-loader',
+                query : {
+                    minimize : true,
+                    removeAttributeQuotes : false
+                }
+            }
         ]
-     },
-     resolve : {
+    },
+    resolve : {
         alias : {
-            node_modules : __dirname + '/node_modules',
-            util : __dirname + '/src/util',
-            page : __dirname + '/src/page',
-            service : __dirname + '/src/service',
-            image : __dirname + '/src/image'
+            node_modules    : __dirname + '/node_modules',
+            util            : __dirname + '/src/util',
+            page            : __dirname + '/src/page',
+            service         : __dirname + '/src/service',
+            image           : __dirname + '/src/image'
         }
-     },
-     plugins:[
-        //通用模块
+    },
+    plugins: [
+        // 独立通用模块到js/base.js
         new webpack.optimize.CommonsChunkPlugin({
             name : 'common',
             filename : 'js/base.js'
         }),
-        //CSS打包
+        // 把css单独打包到文件里
         new ExtractTextPlugin("css/[name].css"),
-        //HTML模板处理
+        // html模板的处理
         new HtmlWebpackPlugin(getHtmlConfig('index', '首页')),
-        new HtmlWebpackPlugin(getHtmlConfig('list', '商品列表页')),
-        new HtmlWebpackPlugin(getHtmlConfig('detail', '商品详情页')),
+        new HtmlWebpackPlugin(getHtmlConfig('list', '商品列表')),
+        new HtmlWebpackPlugin(getHtmlConfig('detail', '商品详情')),
         new HtmlWebpackPlugin(getHtmlConfig('cart', '购物车')),
-        //new HtmlWebpackPlugin(getHtmlConfig('order-confirm', '订单确认页')),
+        new HtmlWebpackPlugin(getHtmlConfig('order-confirm', '订单确认')),
+        new HtmlWebpackPlugin(getHtmlConfig('order-list', '订单列表')),
+        new HtmlWebpackPlugin(getHtmlConfig('order-detail', '订单详情')),
+        new HtmlWebpackPlugin(getHtmlConfig('payment', '订单支付')),
         new HtmlWebpackPlugin(getHtmlConfig('user-login', '用户登录')),
         new HtmlWebpackPlugin(getHtmlConfig('user-register', '用户注册')),
         new HtmlWebpackPlugin(getHtmlConfig('user-pass-reset', '找回密码')),
@@ -85,10 +99,11 @@ var config = {
         new HtmlWebpackPlugin(getHtmlConfig('user-center-update', '修改个人信息')),
         new HtmlWebpackPlugin(getHtmlConfig('user-pass-update', '修改密码')),
         new HtmlWebpackPlugin(getHtmlConfig('result', '操作结果')),
-     ]
+        new HtmlWebpackPlugin(getHtmlConfig('about', '关于Ma&Li')),
+    ]
 };
 
-if(_env === 'dev') {
+if('dev' === WEBPACK_ENV){
     config.entry.common.push('webpack-dev-server/client?http://localhost:4396/');
 }
 
